9f13501fee1a7b5cfa623778feb91fb0243fae7f
BorisKonov <boriskonov23@gmail.com>
2025-12-21 19:13:47 +0200

    Fixing log out blank page error

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('marketplace.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'),
+         name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
